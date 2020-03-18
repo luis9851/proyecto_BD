f4d57d8726fd4199801421f4af78fb5b76e9e995
Edgar <estradacalvilloedgar@gmail.com>
2020-03-17 20:44:27 -0600

    archivos de conf y diseño de front

--- src/index.js
+++ src/index.js
@@ -2,27 +2,30 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
-const session=require('express-session');
+const session = require('express-session');
+
 //Initializations
-const app=express();
+const app = express();
 require('./database');
+
 //Settings
-app.set('port',process.env.PORT || 3000);
-app.set('views',path.join(__dirname,'views'));
-app.engine('.hbs',exphbs({
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
     defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'),'layouts'),
-    partialsDir: path.join(app.get('views'),'partials'),
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
+
 //Middlewares
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(session({
     secret: 'mysecretapp',
     resave: true,
-    saveUninitialized:true
+    saveUninitialized: true
 }));
 
 //Global Variables
@@ -31,9 +34,11 @@ app.use(session({
 app.use(require('./routes/index'));
 app.use(require('./routes/notes'));
 app.use(require('./routes/users'));
+
 //Static Files
 app.use(express.static(path.join(__dirname, 'public')));
+
 //Server is listenning
-app.listen(app.get('port'),()=>{
-    console.log('Server on port',app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send('Index');
+router.get('/', (req, res) => {
+    res.render('index');
 });
-router.get('/about',(req,res)=>{
-    res.send('About');
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 module.exports = router;
