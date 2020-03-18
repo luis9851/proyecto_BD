af8e15ad71c5f0682579dde7f311efa094716492
Luis <loerapasillas12@gmail.com>
2020-03-18 14:55:19 -0600

    prueba4

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add',(req,res)=>{
+    res.render('notes/new-note');
+});
+router.post('/notes/new-note', (req,res)=>{
+    const {title,description}=req.body;
+    const errors=[];
+    if(!title){
+        errors.push({text:'Pleace write a Title'});
+    }
+    if(!description){
+        errors.push({text:'Pleace write a Description'});
+    }
+    if(errors.length>0){
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    }else{
+        res.send('ok');
+    }
+});
 router.get('/notes',(req,res)=>{
     res.send('Notes from database');
 });
