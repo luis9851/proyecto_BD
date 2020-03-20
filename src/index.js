783121c278c9bf766bf20c317fb55e8cc4b28bd5
Luis <loerapasillas12@gmail.com>
2020-03-20 09:41:25 -0600

    Version 3.1.10

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
-    customersDir: path.join(app.get('views'),'customer'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
